55305d8ea0c0c423d3d6a2f8f7afc5b363db0f54
Joy Ceniza <cenizamj@gmail.com>
2023-12-15 11:39:37 +0800

    End of section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -9,7 +9,7 @@ export default class ActivityStore {
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this);
@@ -21,13 +21,13 @@ export default class ActivityStore {
     }
 
     loadActivities = async () => {
+        this.setLoadingInitial(true);
+
         try {
             const activities = await agent.Activities.list();
 
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                // this.activities.push(activity); -> array implementation
-                this.activityRegistry.set(activity.id, activity);
+                this.setActivity(activity);
             });
 
             this.setLoadingInitial(false);
@@ -38,28 +38,46 @@ export default class ActivityStore {
         }
     }
 
+    loadActivity = async (id: string) => {
+        //check if activity is in the registry, else get from API
+        let activity = this.getActivity(id); //variable is declared as 'let': so we can modify the value later
+
+        if(activity) {
+            this.selectedActivity = activity;
+            return activity;
+        } else {
+            this.setLoadingInitial(true);
+
+            try {
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(() => this.selectedActivity = activity);                
+                this.setLoadingInitial(false);
+                return activity;
+            } catch(error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    //declared as private since this will only be used here
+    //this will save an activity into the registry
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        // this.activities.push(activity); -> array implementation
+        this.activityRegistry.set(activity.id, activity);
+    }
+
+    //declared as private since this will only be used here
+    //this gets an activity from the registry
+    private getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
-    }
-
-    selectActivity = (id: string) => {
-        // this.selectedActivity = this.activities.find(a => a.id === id); -> array implementation
-        this.selectedActivity = this.activityRegistry.get(id);
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    openForm = (id?: string) => {
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
-    }
+    }    
 
     createActivity = async (activity: Activity) => {
         this.loading = true;
@@ -124,7 +142,6 @@ export default class ActivityStore {
             runInAction(() => {
                 // this.activities = [...this.activities.filter(x => x.id !== id)]; -> array implementation
                 this.activityRegistry.delete(id);
-                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
             });
         } catch(error) {
